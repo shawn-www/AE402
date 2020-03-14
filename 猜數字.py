200aa8312f7e7552dd864a6429a0bafd2a7ebb9c
shawn-www <62167039+shawn-www@users.noreply.github.com>
2020-03-14 13:43:56 +0800

    Add files via upload

---
+++ 猜數字.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 14:28:19 2020
+
+@author: user
+"""
+
+import random
+
+c=random.randint(1,20)
+
+while c>5:
+a=input
+
+if a ==c:
+    print("www")
+    
+ if a >c:
+     print("www")
+          
+
+
+
